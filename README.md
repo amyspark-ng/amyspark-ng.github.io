369ac5f09f8382eba3dd48580bc090c2788363ec
AmySparkNG ✨ <92493175+amySpark-ng@users.noreply.github.com>
2022-03-14 17:09:03 -0500

    V2.1

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# AmySpark✨'s website thingy kjksdjsl
+# AmySpark's website thingy kjksdjsl ✨
 This project took a lot to do, and im in actual pain! NEVER learning HTML and CSS again i prefer dying :D
 <br>
 <br>
 _though..._
 
-## Credits:
+## — Credits:
 
 _Background Page, Page's favicon and lightbulb:_ - **OMOCAT** ([TW](https://twitter.com/_omocat))
 
@@ -12,7 +12,7 @@ _AmySpark Logo:_ - **DevkyRD** ([TW](https://twitter.com/fabiantol31))
 
 _CSS Help:_ - **LajBel** ([TW](https://twitter.com/lajbel_land))
 
-_Bits of the splash_sfx:_ - From Pureya, game created by **Majorariatto** ([WEBSITE](https://majorariatto.com))
+*Bits of the splash_sfx:* - From Pureya, game created by **Majorariatto** ([WEBSITE](https://majorariatto.com))
 
 _Rest of the page:_ - **me, [AmySpark](https://amySpark-ng.github.io)**
 
@@ -25,5 +25,20 @@ _V1.1:_ Uhhh i honestly don't remember to be fair LOL
 _V2.0:_
 - Changed the presentation text
 - Added dark theme (it was very painful)
-- Some sound effects (lightbulb, vine boom and splash)
+- Some sound effects (easter-egg stuff)
 - Added the "Made with love" text
+
+_V2.1:_
+- Bug on the funny logo not loading
+- Edited README.md to not 'spoil' the easter eggs on the page
+
+## Easter-Eggs:
+<details>
+	<summary>SPOILER i guess</summary>
+	
+- If you click on the PFP thingy it will do an anim (TODO THO LOL)
+
+- If you click on my "logo" it will do a splash anim with its sfx
+
+- If you click my name it will do a vineboom sound effect (very funny)
+</details>
--- assets/external-files/script.js
+++ assets/external-files/script.js
@@ -1,21 +1,5 @@
 window.onload = startFunction()
 
-var darkThemeButton = document.getElementById("bulbImg")
-darkThemeButton.onclick = function() {
-	document.body.classList.toggle("dark-theme")
-
-	if (document.body.classList.contains("dark-theme")) {
-		PlayAudio("assets/audio/lightbulb_off.mp3")
-		darkThemeButton.src = "assets/art/lightBulb.gif"
-	}
-
-	else {
-		PlayAudio("assets/audio/lightbulb_on.mp3")
-		darkThemeButton.src = "assets/art/darkBulb.gif"
-	}
-
-}
-
 function startFunction() {
 	let goloElement = document.getElementById("golo");
 	
@@ -30,42 +14,59 @@ function startFunction() {
 	else {
 		goloElement.src = 'assets/art/amulogo.gif'
 	}
+
+	var darkThemeButton = document.getElementById("bulbImg")
+	darkThemeButton.onclick = function() {
+		document.body.classList.toggle("dark-theme")
+
+		if (document.body.classList.contains("dark-theme")) {
+			PlayAudio("assets/audio/lightbulb_off.mp3")
+			darkThemeButton.src = "assets/art/lightBulb.gif"
+		}
+
+		else {
+			PlayAudio("assets/audio/lightbulb_on.mp3")
+			darkThemeButton.src = "assets/art/darkBulb.gif"
+		}
+
+	}
+
 }
 
 function onClick(elementClicked) {
 	
 	if (elementClicked == "PFP") {
-		let goloElement = document.getElementById("golo");
-			
-		let randomNum = Math.floor(Math.random() * (6 - 1) + 1)
-		console.log(randomNum)
+
+		// let randomNum = Math.floor(Math.random() * (6 - 1) + 1)
+		// console.log(randomNum)
 	
-		switch(randomNum) {
-			case 1:
-			window.open("https://twitter.com/amyspark_ng", "_blank");
-			break;
+		// switch(randomNum) {
+		// 	case 1:
+		// 	window.open("https://twitter.com/amyspark_ng", "_blank");
+		// 	break;
 			
-			case 2:
-			window.open("https://amychan-a.newgrounds.com", "_blank");
-			break;
+		// 	case 2:
+		// 	window.open("https://amychan-a.newgrounds.com", "_blank");
+		// 	break;
 			
-			case 3:
-			window.open("https://github.com/amySpark-ng", "_blank");
-			break;
+		// 	case 3:
+		// 	window.open("https://github.com/amySpark-ng", "_blank");
+		// 	break;
 			
-			case 4:
-			window.open("https://amyspark-ng.itch.io/", "_blank");
-			break;
+		// 	case 4:
+		// 	window.open("https://amyspark-ng.itch.io/", "_blank");
+		// 	break;
 			
-			case 5:
-			funnySpin = true;
-			goloElement.src = 'assets/funyAmulogo.gif'
-			break;
+		// 	case 5:
+		// 	goloElement.src = 'assets/art/funyAmulogo.gif'
+		// 	break;
 			
-			case 6:
-			window.open("https://github.com/amySpark-ng/amySpark-ng.github.io", "_blank");
-			break;
-		}
+		// 	case 6:
+		// 	window.open("https://github.com/amySpark-ng/amySpark-ng.github.io", "_blank");
+		// 	break;
+		// }
+
+		// ^ What originally used to happen, i have some new plans for it now tho so yeah in some time
 	}
 
 	else if (elementClicked == "LOGO") {
